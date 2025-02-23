9eebf752a88eb8dc7d9627ff34d39f2a268c1114
omjain4 <119519103+omjain4@users.noreply.github.com>
2025-02-24 00:09:52 +0530

    Update styles and restructure CSS imports; replace background image in clipped-title

--- React/src/App.js
+++ React/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.js';
 import Contact from './pages/Contact.js';
 import Recruitment from './pages/Recruitment.js';// Use the new page
-import Clients from './components/FeaturedClients.js';
-import './styles/style.css';  
-
+import './styles/base.css';
+import './styles/forms.css';
+import './styles/layout.css';
+import './styles/components.css';
+import './styles/utilities.css';
 function App() {
   return (
     <Router>
